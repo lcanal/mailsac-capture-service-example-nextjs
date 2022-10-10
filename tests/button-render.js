1b2e545c1927a9cb261e82e2c7064849e3d70393
Luis Canales <luis.canales@hey.com>
2022-10-09 22:52:22 -0700

    Fixed chrome implementation

--- tests/button-render.js
+++ tests/button-render.js
@@ -3,7 +3,7 @@ const {Builder, Browser, By, Key, until} = require('selenium-webdriver');
 (async function example() {
   let driver = await new Builder().forBrowser(Browser.CHROME).build();
   try {
-    await driver.get('http://localhost:4444/wd/hub');
+    await driver.get('http://localhost:4444');
     let didSendButtonRender = await driver.findElement(By.id('sendbutton')).isDisplayed()
     if (!didSendButtonRender){
       throw new Error(`Send button was not rendered properly.`);
